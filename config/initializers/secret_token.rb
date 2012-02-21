1b3c8190f3e1e103c49029539b6d9684fd8fd08c
Andy Lenox <andy@livioradio.com>
2012-02-20 22:00:46 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = 'aa77dcc0e2a0394de7b1c2f32a5dd338dc2cf66649a5519316445578762201e2ff741828e3632d29f1db89dcfe9bbb2a72ddb44e3dd60436112c8d1596f9d086'
